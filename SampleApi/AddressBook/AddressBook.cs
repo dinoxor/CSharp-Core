c82f3ab1992ae09836473c17fc496f0acb472e4c
dinoxor <quangmai09@gmail.com>
2019-01-12 00:20:17 -0600

    Add Address Api

---
+++ SampleApi/AddressBook/AddressBook.cs
@@ -0,0 +1,26 @@
+﻿using AddressBook.Model;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+    public class AddressBook
+    {
+        public AddressBook()
+        {
+
+        }
+
+        public List<Contact> GetAllContacts ()
+        {
+
+
+        }
+
+        public void AddContact (Contact contact)
+        {
+            //validation
+            //format first name
+
+        }
+    }
+}
---
+++ SampleApi/AddressBook/Repository/ContactRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using AddressBook.Model;
+
+namespace AddressBook.Repository
+{
+    public class ContactRepository
+    {
+        private string _url;
+
+        public ContactRepository(string url)
+        {
+            _url = url;
+        }
+
+        public List<Contact> Get()
+        {
+            //reads from url
+            //deserialize
+            //returns
+        }
+
+        public int Insert(Contact contact)
+        {
+            //reads from URL
+            //inserts
+            //returns id if successful
+        }
+    }
+}
