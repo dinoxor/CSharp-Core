27e50a47481d4fc9ec2236de56ca12517a1ff68c
dinoxor <quangmai09@gmail.com>
2019-01-21 23:53:25 -0600

    add controllers

---
+++ SampleApi/SampleApi/Controllers/FirstNameController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AddressBook.Model;
+using AddressBook.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstNameController : ControllerBase
+    {
+        private const string url = @"https://www.dropbox.com/s/phffcbuedbir0a3/AddressBookData.txt?dl=1";
+
+        // GET api/firstName
+        [HttpGet]
+        public ActionResult<IEnumerable<Contact>> Get()
+        {
+            var contactRepository = new ContactRepository(url);
+
+            var results = contactRepository.GetAll();
+
+            return results;
+        }
+
+        // GET api/firstName/Sam
+        [HttpGet("{firstName}")]
+        public ActionResult<Contact> Get(string firstName)
+        {
+            var contactRepository = new ContactRepository(url);
+            Contact result;
+
+            try
+            {
+                result = contactRepository.GetByFirstName(firstName);
+            }
+            catch (Exception ex)
+            {
+                return NotFound($"First name not found: \n{ex}");
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ SampleApi/SampleApi/Controllers/IdController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AddressBook.Model;
+using AddressBook.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdController : ControllerBase
+    {
+        //private IRepository _contactRepository;
+
+        private const string url = @"https://www.dropbox.com/s/phffcbuedbir0a3/AddressBookData.txt?dl=1";
+        //public ValuesController(IRepository contactRepository)
+        //{
+        //    _contactRepository = contactRepository;
+        //}
+
+        // GET api/id
+        [HttpGet]
+        public ActionResult<IEnumerable<Contact>> Get()
+        {
+            var contactRepository = new ContactRepository(url);
+
+            var results = contactRepository.GetAll();
+
+            return results;
+        }
+
+        // GET api/id/5
+        [HttpGet("{id}")]
+        public ActionResult<Contact> Get(int id)
+        {
+            var contactRepository = new ContactRepository(url);
+            Contact result;
+
+            try
+            {
+                result = contactRepository.GetById(id);
+            }
+            catch (Exception ex)
+            {
+                return NotFound($"Id not found: \n{ex}");
+            }
+
+            return Ok(result);
+        }
+
+        // POST api/id
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/id/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/id/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
