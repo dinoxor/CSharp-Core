01c2eab1647b326e7b4fa93a0d56d10104e94dd4
dinoxor <quangmai09@gmail.com>
2019-01-27 01:13:14 -0600

    Add request and response Model

---
+++ DueDateService/DueDateService/Model/DueDateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DueDateService.Model
+{
+    public class DueDateRequest
+    {
+        public DateTime DueDate { get; set; }
+        public DateTime CurrentDate { get; set; }
+        public string Frequency { get; set; }
+        public string DueDay1 { get; set; }
+        public string DueDay2 { get; set; }
+    }
+}
---
+++ DueDateService/DueDateService/Model/DueDateResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DueDateService.Model
+{
+    public class DueDateResponse
+    {
+        public int NumberOfMissedPayments { get; set; }
+        public List<DateTime> MissedDates { get; set; }
+    }
+}
