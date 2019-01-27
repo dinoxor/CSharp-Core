36461816051ee1afab1e1d51a975d7e56d0d4e59
dinoxor <quangmai09@gmail.com>
2019-01-27 01:13:55 -0600

    Add Monthly logic

---
+++ DueDateService/DueDateService/Calculator/MonthlyCalculator.cs
@@ -0,0 +1,55 @@
+﻿using DueDateService.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace DueDateService.Calculator
+{
+    internal class MonthlyCalculator : ICalculator
+    {        
+
+        public DueDateResponse Calculate(DueDateRequest request)
+        {
+            var numberOfMissedDates = CalculateNumberOfMissedDates(request);
+
+            return new DueDateResponse
+            {
+                NumberOfMissedPayments = numberOfMissedDates,
+                MissedDates = GetMissedDates(request,numberOfMissedDates)
+            };
+        }
+
+        private int CalculateNumberOfMissedDates(DueDateRequest request)
+        {
+            int monthsDifference = request.CurrentDate.Month - request.DueDate.Month;
+
+            var yearDifferenceInMonths = (request.CurrentDate.Year - request.DueDate.Year) * 12;
+
+            if (request.CurrentDate.Day <= request.DueDate.Day)
+            {
+                monthsDifference--;
+            }
+
+            return monthsDifference + yearDifferenceInMonths;
+        }
+
+        private List<DateTime> GetMissedDates(DueDateRequest request, int numberOfMissedDates)
+        {
+            var missedDueDates = new List<DateTime>();
+
+            if (request.DueDate.Day == DateTime.DaysInMonth(request.DueDate.Year, request.DueDate.Month))
+            {
+                for (int i = 1; i <= numberOfMissedDates; i++)
+                {
+                    var nextMonth = request.DueDate.AddMonths(i);
+
+                    missedDueDates.Add(new DateTime(nextMonth.Year, nextMonth.Month, DateTime.DaysInMonth(nextMonth.Year, nextMonth.Month)));
+                }
+            }
+
+            return missedDueDates;
+
+        }
+    }
+}
