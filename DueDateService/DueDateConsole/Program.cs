da0f2bd91f191d07f7312d66c9db671975c1bd33
dinoxor <quangmai09@gmail.com>
2019-01-27 01:13:38 -0600

    Add Console logic to test dll

--- DueDateService/DueDateConsole/Program.cs
+++ DueDateService/DueDateConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using DueDateService;
+using DueDateService.Model;
 
 namespace DueDateConsole
 {
@@ -6,7 +9,24 @@ namespace DueDateConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var request = new DueDateRequest
+            {
+                DueDate = DateTime.Parse("2019-01-31"),
+                CurrentDate = DateTime.Parse("2019-04-01"),                
+                Frequency = "monthly"
+            };
+
+            var dueDateService = new MissedDateCalculator();
+            var response = dueDateService.Calculate(request);          
+            
+            
+            Debug.WriteLine($"Number of missed payments: {response.NumberOfMissedPayments}");
+
+            Debug.WriteLine("Missed Dates:");
+            foreach (var missedDate in response.MissedDates)
+            {
+                Debug.WriteLine($"{missedDate.ToString("MM/dd/yyyy")}");
+            }
         }
     }
 }
